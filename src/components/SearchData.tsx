9cac2fe101a8154b2860d3c0e02af15ab56eca05
moizghumann <moizghuman@gmail.com>
2023-08-26 17:17:56 +0500

    changed bg color

--- src/components/SearchData.tsx
+++ src/components/SearchData.tsx
@@ -13,7 +13,7 @@ const SearchData = ({ onSearch }: SearchProps) => {
     return (
         <div className='w-full'>
             <input
-                className=' border-[3px] border-[#264653] w-full h-full py-5 mb-12 rounded-3xl pl-3 bg-[#f4a261] text-[#264653] placeholder:italic placeholder:text-[#264653] placeholder:text-lg placeholder:font-semibold placeholder:opacity-60 focus:outline-none'
+                className=' border-[3px] border-[#264653] w-full h-full py-5 mb-12 rounded-3xl pl-3 bg-[#f6bd60] text-[#264653] placeholder:italic placeholder:text-[#264653] placeholder:text-lg placeholder:font-semibold placeholder:opacity-60 focus:outline-none'
                 onChange={handleInputChange}
                 placeholder='Search for name..'
             />
